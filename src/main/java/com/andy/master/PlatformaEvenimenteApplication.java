626327326adfa33dc8be92297623cc1593116b07
AndreeaIonita <ionitaandy@gmail.com>
2025-03-24 12:09:10 +0200

    basic files for the project

---
+++ src/main/java/com/andy/master/PlatformaEvenimenteApplication.java
@@ -0,0 +1,13 @@
+package com.andy.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformaEvenimenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformaEvenimenteApplication.class, args);
+	}
+
+}
